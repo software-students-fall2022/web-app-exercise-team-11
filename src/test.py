eff07b5af868ffc5df0fd42327dcb3c52936d65d
Spectraorder <2421036333@qq.com>
2022-10-19 14:29:49 -0400

    test bootstrap

---
+++ src/test.py
@@ -0,0 +1,22 @@
+import pymongo
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+collection = pymongo.MongoClient('localhost', 27017).admin.test
+
+app = Flask(__name__)
+# Bootstrap(app)
+bootstrap=Bootstrap(app)
+  
+# home page for our application
+@app.route('/')
+def index():
+    return render_template('base.html')
+    # return render_template('home.html')
+
+# d = {"DP510": 12}
+
+# collection.insert_one(d)
+
+if __name__ == '__main__':
+    app.run()
