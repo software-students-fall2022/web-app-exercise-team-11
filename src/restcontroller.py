d80022c7dd8b4023fb1dcc522ae82f3e40fd881b
Spectraorder <2421036333@qq.com>
2022-10-20 15:11:26 -0400

    check page for flights

--- src/restcontroller.py
+++ src/restcontroller.py
@@ -20,19 +20,12 @@ def home():
 # check page for all the flights
 @app.route('/check')
 def check():
-    return render_template('check.html')
-# d1 = datetime(2022, 12, 28, 6, 17, 42)
-# d2 = datetime(2022, 12, 28, 10, 23, 19)
-# d3 = d2-d1
-# s1 = d1.strftime('%Y/%m/%d %H:%M:%S')
-# s2 = d2.strftime('%Y/%m/%d %H:%M:%S')
-# s3 = str(d3)
+    flights = collection.find({})
+    return render_template('check.html', flights=flights)
 
-# d = {"_id":4, "flight": "BY0072P", "aircraft":"A220-300", 
-# "depart_port":"MPTO ", "arrive_port":"KLAX", 
-# "depart_time":s1, "arrive_time":s2, 
-# "duration":s3}
-# collection.insert_one(d)
+all = collection.find({})
+for a in all:
+    print(a['_id'])
 
 if __name__ == '__main__':
     app.run()
