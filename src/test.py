fe4ac2a712b6db281f78e53e7a2502cedf213e6d
Spectraorder <2421036333@qq.com>
2022-10-19 22:06:16 -0400

    jumbotron

--- src/test.py
+++ src/test.py
@@ -1,18 +1,24 @@
+# from django.shortcuts import render
 import pymongo
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_simplelogin import SimpleLogin
 
 collection = pymongo.MongoClient('localhost', 27017).admin.test
 
 app = Flask(__name__)
-# Bootstrap(app) try2
 bootstrap=Bootstrap(app)
+SimpleLogin(app)
   
 # home page for our application
 @app.route('/')
-def index():
-    return render_template('base.html')
-    # return render_template('home.html')
+def home():
+    return render_template('home.html')
+
+# check page for all the flights
+@app.route('/check')
+def check():
+    return render_template('check.html')
 
 # d = {"DP510": 12}
 
