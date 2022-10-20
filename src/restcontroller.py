67721d7631f3cfdd5295af6bfb1814a7190e1aac
Spectraorder <2421036333@qq.com>
2022-10-20 19:43:41 -0400

    edit buttons and corresponding field values

--- src/restcontroller.py
+++ src/restcontroller.py
@@ -1,12 +1,11 @@
-# from django.shortcuts import render
+from re import S
 import pymongo
 import time
 from datetime import date
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
-# collection = pymongo.MongoClient('localhost', 27017).admin.test
 collection = pymongo.MongoClient('mongodb+srv://admin234:274373800@cluster0.o2rcwid.mongodb.net/?retryWrites=true&w=majority').user.admin_user
 
 app = Flask(__name__)
@@ -17,11 +16,20 @@ bootstrap=Bootstrap(app)
 def home():
     return render_template('home.html')
 
-# check page for all the flights
+# check page for all the flights(normal users)
 @app.route('/check')
 def check():
     flights = collection.find({})
     return render_template('normal_check.html', flights=flights)
+
+# check page for all the flights(admin users)
+@app.route('/check_ad', methods=['GET', 'POST'])
+def check_admin():
+    flights = collection.find({})
+    if request.method == 'POST':
+        json_data = request.form
+        print(json_data)
+    return render_template('admin_check.html', flights=flights)
 
 if __name__ == '__main__':
     app.run()
